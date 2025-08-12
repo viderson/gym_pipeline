b4d96040df3dd164652a8ed67a47920b5f825aea
Filip <filip.widera@student.uj.edu.pl>
2025-08-12 17:35:52 +0200

    s

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
     name="database",
@@ -6,16 +22,6 @@ setup(
     description="Pipeline",
     author="FW",
     packages=find_packages(),  
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "plotly",
-        "matplotlib",
-        "seaborn",
-        "jupyter",       
-        "ipykernel",      
-        "nbformat>=4.2.0" 
-    ],
+    install_requires = get_requirements('requirements.txt'),
     python_requires=">=3.8",
 )
